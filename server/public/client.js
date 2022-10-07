74a840ede08b03bbcd02f7200a7c593c21aeeb43
Daniel Legan <legan.daniel@gmail.com>
2022-10-07 17:31:49 -0500

    setup GET request to database

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,27 @@
 console.log('in client.js');
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('in onReady');
+    getToDos();
+}
+
+function getToDos() {
+    console.log('in getToDoes');
+    $('#notDone').empty();
+
+    $.ajax({
+        type: 'GET',
+        url: '/todo'
+    }).then(function (response) {
+        console.log('GET /todo response', response);
+        render();
+    }).catch(function (error) {
+        console.log('GET /todo error', error);
+    });
+}
+
+function render() {
+    console.log('in render');
+}
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -5,6 +5,17 @@ const todoRouter = express.Router();
 const pool = require('../modules/pool');
 
 // GET
+todoRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todo";';
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error getting todos', error);
+        res.sendStatus(500);
+    });
+});
 
 // POST
 
